3551492c2b96ce86e32b9fa7925dfccaa5dfce3d
Yazan <ymasoud@uwaterloo.ca>
2023-10-21 12:58:21 -0400

    Added images to details page

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -5,7 +5,15 @@ import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-details',
-  templateUrl: './details.component.html',
+  template: `
+    <div *ngIf="user">
+      <img [src]="user.picture.large" alt="User Image" loading="lazy">
+      <h1>{{ user.name.title }} {{ user.name.first }} {{ user.name.last }}</h1>
+      <p>Phone: {{ user.phone }}</p>
+      <p>Email: {{ user.email }}</p>
+      <p>Location: {{ user.location.city }}, {{ user.location.state }}, {{ user.location.country }}</p>
+    </div>
+  `,
   styleUrls: ['./details.component.scss']
 })
 export class DetailsComponent implements OnInit {
