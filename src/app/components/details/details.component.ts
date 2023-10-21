91e981fd4c69375d78c1b666169bc91e51d0ca7a
Yazan <ymasoud@uwaterloo.ca>
2023-10-21 14:06:09 -0400

    Fixed pagination mobile view

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -6,6 +6,7 @@ import { User } from '../../models/user.model';
 @Component({
   selector: 'app-details',
   template: `
+  <div class="container" fxLayout="column" fxLayoutAlign="center center" fxFlexFill>
     <div *ngIf="user">
       <img [src]="user.picture.large" alt="User Image" loading="lazy">
       <h1>{{ user.name.title }} {{ user.name.first }} {{ user.name.last }}</h1>
@@ -13,6 +14,7 @@ import { User } from '../../models/user.model';
       <p>Email: {{ user.email }}</p>
       <p>Location: {{ user.location.city }}, {{ user.location.state }}, {{ user.location.country }}</p>
     </div>
+  </div>
   `,
   styleUrls: ['./details.component.scss']
 })
